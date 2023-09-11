c09fb807ce1bf397d9e455008c331fa02476fdcc
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-09-11 18:35:22 +0500

    Task3

---
+++ Tyuiu.DanilovAS.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DanilovAS.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint0.Task3.V0
+{
+    class Program0
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DataService.Sum(7,7));
+            Console.ReadKey();
+        }
+    }
+}
